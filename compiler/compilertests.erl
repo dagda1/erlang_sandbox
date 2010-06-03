607cdb94bbe8a5a918d752e9034cdbb56b9df803
dagda1 <dagda1@scotalt.net>
2010-06-03 14:39:36 +0100

    parser updated to scan a single depth expression.

--- compiler/compilertests.erl
+++ compiler/compilertests.erl
@@ -2,6 +2,7 @@
 -include_lib("eunit/include/eunit.hrl").
 
 compile_test() ->
-	List = lists:concat(parser:scan("(2+3)")),
+	List = parser:scan("(12+3)"),
 	?debugVal(List).
+	%?assertEqual(List, "[12,+,3]").
 	%?assertEqual("{plus, {num, 2}, {num,3}}", parser:scan("(2+3)")).
--- compiler/parser.erl
+++ compiler/parser.erl
@@ -3,21 +3,34 @@
 -export([scan/1]).
 
 scan(Text) ->
-	scan(Text, [], 1, in_text).
-	
-scan([$(|T], Scanned, Column, in_text) ->
-	scan(T, [{open_bracket, "(", Column}|Scanned], Column + 1, open_bracket);	
-scan([$)|T], Scanned, Column, _State) ->	
-	scan(T, [{close_bracket, ")", Column}| Scanned], Column + 1, _State);
-scan([$+|T], Scanned, Column, on_number) -> 
-	scan(T, [{on_plus, "+", Column}|Scanned], Column + 1, on_number);
-scan([H|T], Scanned, Column, open_bracket) when ((H >= $0) and (H =< $9)) ->
-	scan(T, [{on_number, H - 48, Column}|Scanned], Column + 1, on_number);
-scan([H|T], Scanned, Column, on_number) when ((H >= $0) and (H =< $9)) -> 	
-	scan(T, [{on_number, H - 48, Column}|Scanned], Column + 1, on_number);
-scan([], Scanned, Column, _State) ->
-	lists:reverse(lists:map(fun
-								({Identifier, Character, _Pos}) ->
-										?debugVal(Identifier),
-										Character
-							end, Scanned)).
+	scan(Text, [], {0, 1}).	
+scan([$(|T], Scanned, {Depth, Column}) ->
+	scan(T, [{open_bracket, "(", {Depth + 1, Column}}|Scanned], {Depth + 1, Column + 1});	
+scan([$)|T], Scanned, {Depth, Column}) ->	
+	scan(T, [{close_bracket, ")", {Depth, Column}}| Scanned], {Depth - 1, Column + 1});
+scan([$+|T], Scanned, {Depth, Column}) -> 
+	scan(T, [{operator, "+", {Depth, Column}}|Scanned], {Depth, Column + 1});
+scan([H|T], Scanned, {Depth, Column}) when ((H >= $0) and (H =< $9)) ->
+	{Number, Remainder} = get_number([H|T], []),
+	scan(Remainder, [{number, Number, {Depth, Column}}|Scanned], {Depth, Column + 1});
+scan([], Scanned, {_Depth, _Column}) ->
+	%?debugVal(Depth),
+	lists:sort(fun
+				({_LIdentifier, _LCharacter, {LDepth, LColumn}}, {_RIdentifier, _RCharacter, {RDepth, RColumn}})	->
+					if
+						(LColumn < RColumn) ->
+							true;
+						true ->
+							false
+					end														
+	end, Scanned).
+%	lists:reverse(lists:map(fun
+%								({Identifier, Character, {_Depth, _Column}}) ->
+%										%?debugVal(Identifier)
+%										Character
+%							end, Scanned)).
+							
+get_number([H|T], Acc) 	when ((H >= $0) and (H =< $9)) ->
+	get_number(T, [H|Acc]);
+get_number([H|T], Acc) ->
+	{lists:reverse(Acc), [H|T]}.					
