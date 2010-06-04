3e0be17c5fc2ededafbe04df82ee319429fbdaa4
dagda1 <dagda1@scotalt.net>
2010-06-05 00:46:03 +0100

    tests tidied.

--- compiler/compilertests.erl
+++ compiler/compilertests.erl
@@ -1,16 +1,16 @@
 -module(compilertests).	
 -include_lib("eunit/include/eunit.hrl").
 
-%one_digit_test() ->
-%	?assertEqual({num,4}, parser:parse(4)).
+one_digit_test() ->
+	?assertEqual({num,4}, parser:parse(4)).
 
-%simple_parser_test() ->
-%	List = parser:parse("(12+3)"),
-%	?assertEqual({plus,{num,12},{num,3}}, List).
+simple_parser_test() ->
+	List = parser:parse("(12+3)"),
+	?assertEqual({plus,{num,12},{num,3}}, List).
 	
-%complex_parser_test() ->
-%	List = parser:parse("((12+3)-4)"),
-%	?assertEqual({minus, {plus,{num,12},{num,3}}, {num, 4}}, List).
+complex_parser_test() ->
+	List = parser:parse("((12+3)-4)"),
+	?assertEqual({minus, {plus,{num,12},{num,3}}, {num, 4}}, List).
 	
 unary_parser_test() ->
 	List = parser:parse("~((2*3)+(3*4))"),
