072fb0a272a6ea61eef44baa59d9aa5897eb2403
dagda1 <dagda1@scotalt.net>
2010-06-05 00:25:53 +0100

    parser now parses complex expressions like ((2*3)+(3*4)).

--- compiler/parser.erl
+++ compiler/parser.erl
@@ -5,29 +5,43 @@
 parse(Text) ->
 	{Ast, []} = expression(Text),
 	Ast.
-	
-expression(Text) -> sum(Text).
 
-sum(Text) -> 
-	term(Text).
+expression(Text) -> 
+	{Ast, Txt1} = term(Text),
+	adds(Ast, Txt1).
 	
 term(Text) ->
-	{Ast, Txt1} = factor(Text),
-	addops(Ast, Txt1).
+	{Ast, Txt1} = operand(Text),
+	muls(Ast, Txt1).
 	
-factor([$(|Text]) ->
-  {Ast, [$) | Txt1]} = sum(Text),                 
+operand([$(|Text]) ->
+  {Ast, [$) | Txt1]} = expression(Text),                 
   {Ast, Txt1};
-factor([H|T]) when ((H >= $0) and (H =< $9)) ->	
+operand([H|T]) when ((H >= $0) and (H =< $9)) ->	
 	{Number, Remaining} = get_number([H|T], []),
 	{{num, list_to_integer(Number)},Remaining}.
 	
-addops(Ast,[]) -> {Ast, []};
-addops(Ast,[$+|Text]) ->
+muls(Ast, []) -> {Ast, []};
+muls(Ast, [$*|Txt]) ->    
+  {Ast1, Txt1} = operand(Txt),
+  Ast2 = {mul, Ast, Ast1},
+  muls(Ast2, Txt1);
+muls(Ast, [$/|Txt]) ->                         
+  {Ast1, Txt1} = operand(Txt),
+  Ast2 = {dvd, Ast, Ast1},
+  muls(Ast2, Txt1);
+muls(Ast, Txt) -> {Ast, Txt}.	
+	
+adds(Ast,[]) -> {Ast, []};
+adds(Ast,[$+|Text]) ->
 	{Ast1, Txt1} = term(Text),
 	Ast2 = {plus, Ast, Ast1},
-	addops(Ast2, Txt1);
-addops(Ast, Text) -> {Ast, Text}.
+	adds(Ast2, Txt1);
+adds(Ast,[$-|Text]) ->
+	{Ast1, Txt1} = term(Text),
+	Ast2 = {minus, Ast, Ast1},
+	adds(Ast2, Txt1);	
+adds(Ast, Text) -> {Ast, Text}.
 	
 get_number([H|T], Acc) 	when ((H >= $0) and (H =< $9)) ->
 	get_number(T, [H|Acc]);
