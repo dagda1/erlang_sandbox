3e8813d0b727f30fdb0e93f6b438abf59efff4a2
dagda1 <dagda1@scotalt.net>
2010-06-05 01:56:45 +0100

    debug removed from compiler tests.

--- compiler/compilertests.erl
+++ compiler/compilertests.erl
@@ -1,21 +1,21 @@
 -module(compilertests).	
 -include_lib("eunit/include/eunit.hrl").
 
-%one_digit_test() ->
-%	?assertEqual({num,4}, parser:parse(4)).
+one_digit_test() ->
+	?assertEqual({num,4}, parser:parse(4)).
 
-%simple_parser_test() ->
-%	List = parser:parse("(12+3)"),
-%	?assertEqual({plus,{num,12},{num,3}}, List).
+simple_parser_test() ->
+	List = parser:parse("(12+3)"),
+	?assertEqual({plus,{num,12},{num,3}}, List).
 	
-%complex_parser_test() ->
-%	List = parser:parse("((12+3)-4)"),
-%	?assertEqual({minus, {plus,{num,12},{num,3}}, {num, 4}}, List).
+complex_parser_test() ->
+	List = parser:parse("((12+3)-4)"),
+	?assertEqual({minus, {plus,{num,12},{num,3}}, {num, 4}}, List).
 
-%complex_multiply_parser_test() ->
-%	List = parser:parse("((2*3)+(3/4))"),
-%	?debugVal(List),
-%	?assertEqual({plus,{mul,{num,2},{num,3}},{div,{num,3},{num,4}}}, List).
+complex_multiply_parser_test() ->
+	List = parser:parse("((2*3)+(3/4))"),
+	?debugVal(List),
+	?assertEqual({plus,{mul,{num,2},{num,3}},{dvd,{num,3},{num,4}}}, List).
 	
 unary_parser_test() ->	
 	List = parser:parse("~((2*3)+(3*4))"),
